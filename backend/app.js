32635897c135ab473cf9404a2ab56d3ad6ed41c9
Jayce <jayce.kuruvilla@gmail.com>
2024-06-27 17:24:20 +0530

    CORS

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.use(cors({
-    origin: ['https://kkem-blog-test-mern-client-130cby4ua-jayces-projects-f5b147c5.vercel.app'],
+    origin: ['https://kkem-blog-test-mern-client-j2hqhzkue-jayces-projects-f5b147c5.vercel.app'],
     credentials: true,
     methods: ['POST', 'GET']
   }));
